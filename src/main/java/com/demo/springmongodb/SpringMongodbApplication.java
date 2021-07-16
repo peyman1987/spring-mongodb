d7b6f6e614ac1e64283541fa9e06d97c75685839
ISC024\ISC_024 <peyman_em@yahoo.com>
2021-07-16 22:32:45 +0200

    Edit properties

--- src/main/java/com/demo/springmongodb/SpringMongodbApplication.java
+++ src/main/java/com/demo/springmongodb/SpringMongodbApplication.java
@@ -1,10 +1,21 @@
 package com.demo.springmongodb;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 public class SpringMongodbApplication {
+
+	public static Logger logger = LoggerFactory.getLogger(SpringMongodbApplication.class);
+
+	@PostConstruct
+	public void getLogger() {
+		logger.info("Application started...");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringMongodbApplication.class, args);
